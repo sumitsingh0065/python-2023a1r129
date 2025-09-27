e154c49451c9645bc8721f1b724298f9b8ef4eeb
sumitsingh0065 <152257946+sumitsingh0065@users.noreply.github.com>
2025-09-27 21:09:26 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,26 @@
+#  'james'   "james"   '''james'''      >str
+#            [12,23,34]     .List
+#            (12,23,34)     >tuple
+#            {12,23,34}     >set
+#            {1:3, 4:5, 6:7}    dict. 
+
+# Lst=List()
+# Lst=[]
+# Print(lst)
+
+# #   0  1  2  3  4
+# Lst=[10,20,30,40,50]
+# #   -5  -4  -3  -2  -1
+# Print(lst[0],lst[-1])  /   Print(lst[0:3])     /   Print(lst[-3:])   /  Print(lst[::-1]) 
+
+
+# St=["james","neena","king","blake","paul"]
+# # Print(st[-2])    /      Print(st[-2][-2])     /  id(st[0])
+# for name in St:
+#     print(name.upper())
+
+
+st="hello"
+st[0]="j"
+print(st)
+
