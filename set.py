ea1aaa5e8f3c21412c2bf4b429c8fe5b8a94eb53
sumitsingh0065 <152257946+sumitsingh0065@users.noreply.github.com>
2025-10-07 12:52:11 +0530

    Create set.py

---
+++ set.py
@@ -0,0 +1,15 @@
+# fac={"c","cpp","java","php"}
+# stu={"c","cpp","js","python"}
+
+# # res=fac | stu
+# # res=stu-fac
+# # res=stu^fac
+# res=fac&stu
+# # stu|=fac
+# print(res)
+
+lst=["red","green","blue","green","red"]
+# s=set(lst)
+# print(s)
+lst=frozenset(["red","green","blue","green","red"])
+print(lst)
