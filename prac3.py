5cf75e833253003fdb1b5fbe8297590f50969783
Sumit Singh <152257946+sumitsingh0065@users.noreply.github.com>
2025-10-16 22:35:18 +0530

    Create prac3.py

---
+++ prac3.py
@@ -0,0 +1,10 @@
+def fibonacci(n):
+    a, b = 0, 1
+    seq = []
+    for _ in range(n):
+        seq.append(a)
+        a, b = b, a + b
+    return seq
+
+num = int(input("Enter number of terms: "))
+print(fibonacci(num))
